e218b9379ba012352efdbf350627a56d04fe4065
Pawel Kosmatka <pawel.kosmatka@outlook.com>
2019-05-31 15:13:17 +0200

    Implementing LZW using streams

--- LZWCompression/Program.cs
+++ LZWCompression/Program.cs
@@ -12,16 +12,29 @@ namespace LZWCompression
         static void Main(string[] args)
         {
             var entryPath = AppDomain.CurrentDomain.BaseDirectory + "entry.txt";
-            var compressedPath = AppDomain.CurrentDomain.BaseDirectory + "compressed.txt";
+            var compressedPath = AppDomain.CurrentDomain.BaseDirectory + "compressed";
             var decompressedPath = AppDomain.CurrentDomain.BaseDirectory + "decompressed.txt";
 
             Compress(entryPath, compressedPath);
             Decompress(compressedPath, decompressedPath);
 
+            var entryFileSize = new FileInfo(entryPath).Length;
+            var compressedFileSize = new FileInfo(compressedPath).Length;
+            var decompressedFileSize = new FileInfo(decompressedPath).Length;
+
+            Console.WriteLine("File sizes [B]:");
+            Console.WriteLine($"Source: \t{entryFileSize}");
+            Console.WriteLine($"Compressed: \t{compressedFileSize}");
+            Console.WriteLine($"Decompressed: \t{decompressedFileSize}");
+            Console.WriteLine("\nCompression ratio:");
+            Console.WriteLine(((double)entryFileSize / compressedFileSize).ToString("F"));
+            
+            Console.ReadLine();
         }
 
         public static void Compress(string entryPath, string compressedPath)
         {
+
             var dictionary = new Dictionary<string, int>();
             for (byte i = 0; i < byte.MaxValue; i++)
             {
@@ -32,9 +45,13 @@ namespace LZWCompression
 
             var c = ((char)source.Read()).ToString();
             char s;
-            var result = new FileStream(compressedPath, FileMode.OpenOrCreate, FileAccess.Write);
-            //var result = new StreamWriter(compressedPath, false, Encoding.UTF8);
-            var x = BitConverter.GetBytes(dictionary[c]);
+            var result = new FileStream(compressedPath, FileMode.Create, FileAccess.Write);
+            Action<int> writeByte = (n) =>
+            {
+                result.WriteByte((byte)(n >> 8));
+                result.WriteByte((byte)n);
+            };
+
             while (!source.EndOfStream)
             {
                 s = (char)source.Read();
@@ -45,8 +62,7 @@ namespace LZWCompression
                 }
                 else
                 {
-                    result.Write(BitConverter.GetBytes(dictionary[c]), 0, 4);
-                    //result.Write(dictionary[c] + ",");
+                    writeByte(dictionary[c]);
                     dictionary.Add(cs, dictionary.Count);
                     c = s.ToString();
                 }
@@ -54,12 +70,13 @@ namespace LZWCompression
 
             if (!string.IsNullOrEmpty(c))
             {
-                //result.Write(dictionary[c] + ",");
+                writeByte(dictionary[c]);
             }
-
+            
             source.Dispose();
             result.Dispose();
         }
+
 
         public static void Decompress(string compressedPath, string decompressedPath)
         {
@@ -69,16 +86,21 @@ namespace LZWCompression
                 dictionary.Add(i, ((char)i).ToString());
             }
 
-            var source = new StreamReader(compressedPath, Encoding.UTF8);
+            var source = new FileStream(compressedPath, FileMode.Open, FileAccess.Read);
+            Func<int> readCode = () =>
+            {
+                var buffer = source.ReadByte();
+                return (source.ReadByte()) | (buffer << 8);
+            };
 
-            var fc = getCode();
+            var result = new StreamWriter(decompressedPath, false);
 
-            var result = new StreamWriter(decompressedPath, false, Encoding.UTF8);
+            var fc = readCode();
             result.Write(dictionary[fc]);
 
-            while (!source.EndOfStream)
+            while (source.Position != source.Length)
             {
-                var c = getCode();
+                var c = readCode();
                 var pc = dictionary[fc];
                 if (dictionary.ContainsKey(c))
                 {
@@ -90,24 +112,10 @@ namespace LZWCompression
                     dictionary.Add(dictionary.Count, pc + pc[0]);
                     result.Write(pc + pc[0]);
                 }
-
                 fc = c;
-
             }
             source.Dispose();
             result.Dispose();
-
-            int getCode()
-            {
-                var entry = source.Read();
-                string code = string.Empty;
-                while ((char)entry != ',')
-                {
-                    code += (char)entry;
-                    entry = source.Read();
-                }
-                return int.Parse(code);
-            }
         }
 
     }
